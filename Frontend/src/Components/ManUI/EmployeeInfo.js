5c1fd73d3d05ee8df9c54ff0492bc84a265ead72
Sithika Guruge <sithikaguruge2001@gmail.com>
2023-10-17 15:11:41 +0530

    Personal info page fixed

--- Frontend/src/Components/ManUI/EmployeeInfo.js
+++ Frontend/src/Components/ManUI/EmployeeInfo.js
@@ -1,102 +1,76 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import bootsrap from "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const EmployeeInfo = () => {
-  const [column, setColumn] = useState([]);
-  const [record, setRecord] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [records, setRecords] = useState([]);
   const navigate = useNavigate();
+
   const goBack = () => {
     navigate(-1);
   };
+
   useEffect(() => {
     fetch("http://localhost:5001/api/employeeInfo")
       .then((response) => response.json())
       .then((data) => {
-        setColumn(Object.keys(data[0]));
-        setRecord(data);
+        setColumns(Object.keys(data[0]));
+        setRecords(data);
       })
       .catch((error) => console.error("Error fetching data2:", error));
   }, []);
 
   return (
     <div>
-      <div className="d-flex flex-column align-items-center gradient-bg bg-primary vh-200">
+      <div className="d-flex flex-column align-items-center gradient-bg bg-primary vh-100">
         <h1 style={{ marginBottom: "20px", marginTop: "20px" }}>
-          Employee Informations
+          Employee Information
         </h1>
-        {record.map((record) => (
-          <div
-            style={{
-              border: "2px solid black",
-              padding: "20px",
-              marginTop: "10px",
-              marginBottom: "10px",
-              borderRadius: "10px",
-              width: "45%",
-              display: "flex",
-              flexDirection: "column", // Align children vertically
-              alignItems: "center", // Center children horizontally
-              // Center children horizontally
-            }}
-          >
-            <div>
-              <h5>Employee ID : {record.Employee_ID}</h5>
-              <h5>Name : {record.Name}</h5>
-              <Link
-                to="/login/Employee/ManUI/ViewEmployee"
-                style={{ color: "white" }}
-              >
-                <button
-                  className="btn btn-success"
-                  onClick={() => {
-                    fetch("http://localhost:5001/api/send-employee-id", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({ Employee_ID: record.Employee_ID }),
-                    })
-                      .then((data) => {
-                        console.log("success");
-                      })
-                      .catch((error) => {
-                        console.log("error");
-                      });
-                  }}
-                  style={{ marginRight: "5px" }}
-                >
-                  View
-                </button>
-              </Link>
-              <Link
-                to="/login/Employee/ManUI/EditEmployee"
-                style={{ color: "white" }}
-              >
-                <button
-                  className="btn btn-warning"
-                  onClick={() => {
-                    fetch("http://localhost:5001/api/send-employee-id", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({ Employee_ID: record.Employee_ID }),
-                    })
-                      .then((data) => {
-                        console.log("success");
-                      })
-                      .catch((error) => {
-                        console.log("error");
-                      });
-                  }}
-                >
-                  Edit
-                </button>
-              </Link>
-            </div>
-          </div>
-        ))}
+        <table className="table table-bordered" style={{ width: "70%" }}>
+          <thead className="thead-dark">
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index}>{column}</th>
+              ))}
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record, index) => (
+              <tr key={index}>
+                {columns.map((column, index) => (
+                  <td key={index}>{record[column]}</td>
+                ))}
+                <td>
+                  <Link
+                    to="/login/Employee/ManUI/ViewEmployee"
+                    style={{ color: "white" }}
+                  >
+                    <button
+                      className="btn btn-success"
+                      onClick={() => handleButtonClick(record.Employee_ID)}
+                      style={{ marginRight: "5px" }}
+                    >
+                      View
+                    </button>
+                  </Link>
+                  <Link
+                    to="/login/Employee/ManUI/EditEmployee"
+                    style={{ color: "white" }}
+                  >
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => handleButtonClick(record.Employee_ID)}
+                    >
+                      Edit
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <div style={{ display: "flex", justifyContent: "center" }}>
           <button
             onClick={goBack}
@@ -113,6 +87,22 @@ const EmployeeInfo = () => {
       </div>
     </div>
   );
+
+  function handleButtonClick(employeeId) {
+    fetch("http://localhost:5001/api/send-employee-id", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ Employee_ID: employeeId }),
+    })
+      .then((data) => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  }
 };
 
 export default EmployeeInfo;
